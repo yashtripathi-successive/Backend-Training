64e3c007c6bcb9877e188b5e098439037f502dd9
Yash Tripathi <yash.tripathi@parthvrs-29.successivetech.com>
2025-07-29 12:03:27 +0530

    Assignment 12 Done

--- src/utils/securityHeader.ts
+++ src/utils/securityHeader.ts
@@ -5,8 +5,6 @@ class securityHeaderClass{
 public securityHeader(req:Request,res:Response,next:NextFunction){
      
       res.setHeader('X-Developed-By', 'Yash-Tripathi'); 
-      res.setHeader('Referrer-Policy', 'no-referrer');
-      res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
       next();
 }
 }
